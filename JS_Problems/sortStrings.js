4a1089eed7dba34aabcf7a8ce25a872f320977ab
Jay011999 <jay483338@gmail.com>
2024-01-03 01:45:24 +0530

    string sort

---
+++ JS_Problems/sortStrings.js
@@ -0,0 +1,13 @@
+let newString='kdfjdffiwdaskxnewucwejiwojwc';
+
+
+
+console.log(newString.split('').sort().join(''));
+
+//function
+
+function sortedString(stringValue){
+    return stringValue.split('').sort().join('');
+}
+
+console.log(sortedString('nvbnbmvcnbmiufierufj'));
