b7bede32931caeda7ec9c04afea8c0913a3773ac
Jay011999 <jay483338@gmail.com>
2024-01-04 19:40:39 +0530

    sorting numers array

---
+++ JS_Problems/sortingAnArray.js
@@ -0,0 +1,11 @@
+// return numbers only
+let randomArray=[575,646,4454,456563,3535454,6565,45,67,544];
+
+function sortedAsscendingOrder(){
+  return randomArray.sort((a,b)=>{
+    return a-b
+  }); 
+}
+console.log(sortedAsscendingOrder());
+
+
