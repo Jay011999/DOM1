d8b1dd5a4155bb2ace8980d50aeb12d1eac842d2
Jay011999 <jay483338@gmail.com>
2024-01-04 18:57:17 +0530

    add number decreasing

--- JS_Problems/addUpNumbers.js
+++ JS_Problems/addUpNumbers.js
@@ -1,10 +1,11 @@
 //pass and recursive
 
-function returnSumofNum(num){
-    if(num==0){
-      return num;
-    }
-    return num+ returnSumofNum(num-1);
+function returnSumofNum(num) {
+  if (num == 0) {
+    return num;
+  } else {
+    return num + returnSumofNum(num - 1);
   }
-  
-  console.log(returnSumofNum(10));
+}
+
+console.log(returnSumofNum(10));
